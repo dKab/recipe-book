cf55e9f89116248071d9dcd01375deaae54a72b1
dmitriy <dkabardinov@gmail.com>
2017-02-18 11:59:24 +0300

    📦switch back from express to koa - hmr finally works with koa

--- src/server/server.js
+++ src/server/server.js
@@ -1,25 +1,27 @@
 const webpackDevConfig = require('../../config/webpack.config.dev.js');
-const webpackMiddleware = require('webpack-dev-middleware');
+const webpackMiddleware = require('koa-webpack');
 const webpack = require('webpack');
-const express = require('express');
-const webpackHotMiddleware = require('webpack-hot-middleware');
+const koa = require('koa');
 const path = require('path');
+const Router = require('koa-router');
 
+const router = new Router();
 const compiler = webpack(webpackDevConfig());
+const serve = require('koa-static');
+const app = new koa();
 
-const app = express();
-
-app.get('/', function(req,res) {
-  res.sendFile(path.resolve(__dirname, '../../public/index.html'));
+router.get('/', function(ctx) {
+  ctx.res.sendFile(path.resolve(__dirname, '../../public/index.html'));
 });
 
-app.use(express.static('public'));
+app.use(serve('public')); //serve assets from public folder
 
 console.log(`running app in ${process.env.NODE_ENV} mode`);
 
 if (process.env.NODE_ENV != 'prod') {
-    app.use(webpackMiddleware(compiler));
-    app.use(webpackHotMiddleware(compiler));
+    app.use(webpackMiddleware({
+        compiler: compiler
+    }));
 }
 
 app.listen(3001, function () {
