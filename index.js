089cf3d30be238f89517c31a23bdc0bac2f7f426
dmitriy <dkabardinov@gmail.com>
2017-02-23 20:57:16 +0300

    Bring redux into play

--- index.js
+++ index.js
@@ -2,13 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {AppContainer} from 'react-hot-loader';
 import {RootComponent} from './components/RootComponent';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import {recipiesReducer} from './reducers';
+
+const preloadedState = window.__PRELOADED_STATE__;
+
+delete window.__PRELOADED_STATE__
+
+const store = createStore(recipiesReducer, preloadedState);
 
 const render = (Component) => {
-    let components = process.env.NODE_ENV != 'production'
-        ? (<AppContainer>
-                <Component/>
-            </AppContainer>)
-        : <Component />;
+    let components;
+    if (process.env.NODE_ENV === 'production') {
+        components = <Provider store={store}><Component /></Provider>;
+    } else {
+        components = (<AppContainer>
+                        <Provider store={store}><Component/></Provider>
+                      </AppContainer>);
+    }
     ReactDOM.render(
         components,
         document.getElementById('react-root')
