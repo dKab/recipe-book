e53eba5a8e7a7831d681b70aec390c52f3734102
Sergey Kab <greenabomination@gmail.com>
2017-03-18 22:36:06 +0500

    deps crash

---
+++ config/dbconfig.js
@@ -0,0 +1,7 @@
+module.exports = {
+  user: "postgres",
+  database: "postgres",
+  password: "openthedog",
+  host: "localhost",
+  port: 5432
+};
--- server/getRecipes.js
+++ server/getRecipes.js
@@ -1,9 +1,31 @@
 import fs from "fs";
 import path from "path";
+import pg from "pg";
+const config = {
+  user: "postgres",
+  database: "postgres",
+  password: "openthedog",
+  host: "localhost",
+  port: 5432,
+  max: 10,
+  idleTimeoutMillis: 30000
+};
+const pool = new pg.Pool(config);
 
 export const getRecipes = () => {
   // TODO replace it with actual query to DB once db is set up
-  return loadDataFromFile(path.resolve(__dirname, "../__mocks__/recipes.json"));
+  // console.log(dbconfig);
+
+  // return loadDataFromFile(path.resolve(__dirname, "../__mocks__/recipes.json"));
+  return new Promise((resolve, reject) => {
+    pool.query(`SELECT * FROM public."Recipes";`, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result.rows);
+      }
+    });
+  });
 };
 
 const loadDataFromFile = mockFileName => {
@@ -28,4 +50,4 @@ function readModuleFile(pathToFile, callback) {
   } catch (e) {
     callback(e);
   }
-}
+};
